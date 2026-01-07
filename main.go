694bb74f55b0a8ba50b1d5376ddf6d1ab773009c
sean yu <55464069+hexbabe@users.noreply.github.com>
2026-01-07 14:08:13 -0500

    RSDK-12749 — Allow find-webcams on darwin to update devices during runtime (#7)
    * Bump to new dep and use
    
    * Remove webcam registration
    
    * Move manager drivers clear into linux conditional; Fix go.mod

--- main.go
+++ main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"find-webcams/models"
+	"os"
+	"runtime"
 
+	mdcam "github.com/pion/mediadevices/pkg/driver/camera"
+	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/services/discovery"
 )
 
+var logger = logging.NewDebugLogger("find-webcams-entrypoint")
+
 func main() {
-	module.ModularMain(resource.APIModel{discovery.API, models.WebcamDiscovery})
+	if runtime.GOOS == "darwin" {
+		if err := mdcam.StartObserver(); err != nil {
+			logger.Errorw("failed to start camera observer", "error", err)
+			os.Exit(1)
+		}
+		defer func() {
+			if err := mdcam.DestroyObserver(); err != nil {
+				logger.Errorw("failed to destroy camera observer", "error", err)
+			}
+		}()
+	}
+
+	module.ModularMain(
+		resource.APIModel{
+			API:   discovery.API,
+			Model: models.WebcamDiscovery,
+		},
+	)
 }
